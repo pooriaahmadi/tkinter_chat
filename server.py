a6064a3c95944fd833ab64aa4e8b90e0acbe75ba
Pooria ahmadi <pooriababak444@gmail.com>
2021-04-05 13:29:07 +0430

    Update server.py

--- server.py
+++ server.py
@@ -34,6 +34,17 @@ def get_connections():
         clients_list[user['username']] = user
         Thread(target=incomming_messages, args=(client_socket,)).start()
         log(f'Connection from {address[0]}:{address[1]} has been established!').info()
+        user = {
+            'username': user['username'],
+        	'message': 'User Has Joined The Chat!'
+        }
+        user = pickle.dumps(user)
+        for client in clients_list:
+            client = clients_list[client]
+            try:
+                client['socket'].send(encode(create_header(user))+user)
+            except BrokenPipeError:
+            	del clients_list[client]
 Thread(target=get_connections).start()
 
 def incomming_messages(client_socket):
@@ -50,7 +61,10 @@ def incomming_messages(client_socket):
             user = pickle.dumps(user)
             for client in clients_list:
                 client = clients_list[client]
-                client['socket'].send(encode(create_header(user))+user)
+                try:
+                    client['socket'].send(encode(create_header(user))+user)
+                except:
+                    pass
 while True:
     message = input('Send message to all: ')
     user = {
@@ -60,4 +74,7 @@ while True:
     user = pickle.dumps(user)
     for client in clients_list:
         client = clients_list[client]
-        client['socket'].send(encode(create_header(user))+user)
+        try:
+            client['socket'].send(encode(create_header(user))+user)
+        except BrokenPipeError:
+            pass
